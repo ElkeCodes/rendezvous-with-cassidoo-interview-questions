1fd0209b667390a4bed09a37efdb2af64fcd98b2
Elke Heymans <el.heymans@gmail.com>
2025-03-31 20:28:38 +0200

    Day 0001

---
+++ src/days/0001.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest";
+import { toRomanNumeral } from "./0001";
+
+describe("day #0001", () => {
+  test("toRomanNumeral", () => {
+    expect(toRomanNumeral(1)).toBe("I");
+    expect(toRomanNumeral(2)).toBe("II");
+    expect(toRomanNumeral(4)).toBe("IV");
+    expect(toRomanNumeral(10)).toBe("X");
+    expect(toRomanNumeral(415)).toBe("CDXV");
+    expect(toRomanNumeral(484)).toBe("CDLXXXIV");
+    expect(toRomanNumeral(641)).toBe("DCXLI");
+    expect(toRomanNumeral(1000)).toBe("M");
+    expect(toRomanNumeral(999)).toBe("CMXCIX");
+  });
+});
