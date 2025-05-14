184723823e5dc6630153b6b848e9ec8c29f8b883
Elke Heymans <el.heymans@gmail.com>
2025-05-14 20:00:42 +0200

    Day 0404

--- src/days/0404-add-operators/add-operators.spec.ts
+++ src/days/0404-add-operators/add-operators.spec.ts
@@ -3,12 +3,18 @@ import { addOperators } from "./add-operators";
 
 describe("day #0404", () => {
   test("examples", () => {
-    expect(addOperators(123, 6)).toEqual(
-      expect.arrayContaining(["1*2*3", "1+2+3"]),
-    );
-    expect(addOperators(123, 9)).toEqual(
-      expect.arrayContaining(["1*2*3", "1+2+3"]),
-    );
+    expect(addOperators(123, 6)).toStrictEqual(["1+2+3", "1*2*3"]);
     expect(addOperators(3456237490, 9191)).toStrictEqual([]);
+  });
+  test("debug", () => {
+    expect(addOperators(1234, 15)).toStrictEqual(["1+2+3*4"]);
+    expect(addOperators(23431, 12)).toStrictEqual([
+      "2+3+4+3*1",
+      "2+3+4+3/1",
+      "2+3*4-3+1",
+      "2-3+4*3+1",
+      "2*3+4+3-1",
+    ]);
+    expect(addOperators(13425, -6)).toStrictEqual(['1+3-4/2*5'])
   });
 });
--- src/days/0404-add-operators/add-operators.ts
+++ src/days/0404-add-operators/add-operators.ts
@@ -1,38 +1,27 @@
 export function addOperators(origin: number, target: number): Array<string> {
-  let [tempResult, ...remainder] = origin
+  let [result, ...remainder] = origin
     .toString()
     .split("")
     .map((x) => parseInt(x, 10));
-  let queue = [{ remainder, tempResult, result: `${tempResult}` }];
-  const operators: Array<
-    (
-      a: number,
-      b: number,
-      result: string,
-    ) => { newTempResult: number; finalResult: string }
-  > = [
-    (a, b, result) => ({ newTempResult: a + b, finalResult: `${result}+${b}` }),
-    (a, b, result) => ({ newTempResult: a - b, finalResult: `${result}-${b}` }),
-    (a, b, result) => ({ newTempResult: a * b, finalResult: `${result}*${b}` }),
-    (a, b, result) => ({ newTempResult: a / b, finalResult: `${result}/${b}` }),
+  let queue = [{ remainder, result: `${result}` }];
+  const operators: Array<(digit: number, result: string) => string> = [
+    (digit, result) => `${result}+${digit}`,
+    (digit, result) => `${result}-${digit}`,
+    (digit, result) => `${result}*${digit}`,
+    (digit, result) => `${result}/${digit}`,
   ];
   let results: Array<string> = [];
   while (queue.length > 0) {
-    let { remainder, tempResult, result } = queue.shift()!;
+    let { remainder, result } = queue.shift()!;
     const firstDigit = remainder[0];
     const newRemainder = remainder.slice(1);
     operators.forEach((operator) => {
-      const { newTempResult, finalResult } = operator(
-        tempResult,
-        firstDigit,
-        result,
-      );
-      if (newTempResult === target) {
+      const finalResult = operator(firstDigit, result);
+      if (newRemainder.length === 0 && eval(finalResult) === target) {
         results.push(finalResult);
       } else if (newRemainder.length > 0) {
         queue.push({
           remainder: newRemainder,
-          tempResult: newTempResult,
           result: finalResult,
         });
       }
