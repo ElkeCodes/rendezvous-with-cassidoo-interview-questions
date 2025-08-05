ec416e894ff642ba3aa3655a41a3024af09b7429
Elke Heymans <el.heymans@gmail.com>
2025-08-05 06:43:47 +0200

    Simplified 0397

--- src/days/0397-longest-true-streak/find-longest-streak.ts
+++ src/days/0397-longest-true-streak/find-longest-streak.ts
@@ -1,20 +1,9 @@
-export function findLongestStreak(
-  candidates: Array<boolean>,
-  minLength: number,
-): number {
-  let maxStreak = 0;
-  let currentStreak = 0;
-  candidates.forEach((currentCandidate) => {
-    if (currentCandidate) {
-      currentStreak++;
-    } else {
-      maxStreak = Math.max(maxStreak, currentStreak);
-      currentStreak = 0;
-    }
+export function findLongestStreak(data: Array<boolean>, goal: number): number {
+  let max = 0,
+    current = 0;
+  data.forEach((truthyValue) => {
+    current = truthyValue ? current + 1 : 0;
+    max = Math.max(max, current);
   }, 0);
-  maxStreak = Math.max(maxStreak, currentStreak);
-  if (maxStreak >= minLength) {
-    return maxStreak;
-  }
-  return 0;
+  return max >= goal ? max : 0;
 }
