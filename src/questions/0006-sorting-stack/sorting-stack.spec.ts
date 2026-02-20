c13c344ddac287687ec208770bf514ce47214eb6
Elke Heymans <el.heymans@gmail.com>
2026-02-20 09:43:26 +0100

    More testcases for 0006

--- src/questions/0006-sorting-stack/sorting-stack.spec.ts
+++ src/questions/0006-sorting-stack/sorting-stack.spec.ts
@@ -25,4 +25,30 @@ describe("question #0006", () => {
     expect(sortedStackDesc.pop()).toBe(2);
     expect(sortedStackDesc.pop()).toBe(1);
   });
+
+  test("if the stack can be sorted descending with duplicate values", () => {
+    const stack = new Stack<number>();
+    stack.push(3);
+    stack.push(1);
+    stack.push(2);
+    stack.push(2);
+    
+    const sortedStackDesc = sortStack(stack, "desc");
+    expect(sortedStackDesc.pop()).toBe(3);
+    expect(sortedStackDesc.pop()).toBe(2);
+    expect(sortedStackDesc.pop()).toBe(2);
+    expect(sortedStackDesc.pop()).toBe(1);
+  }); 
+
+  test("if the stack can be sorted ascending with string values", () => {
+    const stack = new Stack<string>();
+    stack.push("banana");
+    stack.push("apple");
+    stack.push("cherry");
+
+    const sortedStackAsc = sortStack(stack, "asc");
+    expect(sortedStackAsc.pop()).toBe("apple");
+    expect(sortedStackAsc.pop()).toBe("banana");
+    expect(sortedStackAsc.pop()).toBe("cherry");
+  });
 });
